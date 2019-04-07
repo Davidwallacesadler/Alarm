94c83cee0c19d1397858f1c80bf37e597216ee68
David Sadler <davidwallacesadler@gmail.com>
2019-04-07 11:22:35 -0600

    Set up master detail views with view elements and actions

---
+++ Alarm/View/AlarmTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  AlarmTableViewCell.swift
+//  Alarm
+//
+//  Created by David Sadler on 4/7/19.
+//  Copyright © 2019 David Sadler. All rights reserved.
+//
+
+import UIKit
+
+class AlarmTableViewCell: UITableViewCell {
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
